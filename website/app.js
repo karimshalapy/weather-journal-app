f5c07f6a86a1a79b88a7bc439ed9e7c32bb45d8a
Karim <karimshalapy@gmail.com>
2020-08-11 15:09:26 +0200

    Added the functionality for the client side code

--- website/app.js
+++ website/app.js
@@ -1,5 +1,72 @@
 /* Global Variables */
-
+const domObj = {
+    submitBtn: document.getElementById("generate"),
+    zipInput: document.getElementById("zip"),
+    feelingsInput: document.getElementById("feelings"),
+    lastEntry: document.getElementById("last-entry"),
+}
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+/////////////////////////////////////////////////////////////////////////////////
+/*Helper Functions*/
+//get the data from the api according to the user's zip code input
+const getWeather = async (zipCode) => {
+    const API_KEY = "225e96ba4490c4ecf1c6811dc08511b0";
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${API_KEY}`)
+    try {
+        const weather = await response.json();
+        return weather
+    } catch (err) {
+        console.log(err)
+    }
+}
+//get the input of the user and the data from API and return an entry object
+const getEntry = async () => {
+    const feeling = domObj.feelingsInput.value;
+    const zipCode = domObj.zipInput.value;
+    const weatherObj = await getWeather(zipCode);
+    return {
+        feeling,
+        zipCode,
+        weather: Math.round(weatherObj.main.temp - 273.15),
+        date: newDate,
+    }
+}
+//render the entry data on the DOM
+const renderData = (entry) => {
+    let domString;
+    //incase the input was incorrect or the response is 404
+    if (!entry) {
+        domString = `
+        <h2>Most Recent Entry</h2>
+        <p> 404 city not found...Wrong zip code entered. </p>
+        `
+        //if the input was correct and got a proper response
+    } else {
+        domString = `
+        <h2>Most Recent Entry</h2>
+        <div id="date"> <p> Date: ${entry.date} </p> </div>
+        <div id="temp"> <p> Temprature: ${entry.weather}<sup>o</sup>C </p> </div>
+        <div id="content"> 
+        <p> Zip Code: ${entry.zipCode} </p>
+        <p> Feelings: ${entry.feeling} </p>
+        </div>
+        `;
+    }
+    domObj.lastEntry.innerHTML = domString;
+};
+/////////////////////////////////////////////////////////////////////////////////
+/*Set-up The Event Listener*/
+domObj.submitBtn.addEventListener("click", async e => {
+    //prevent the main action
+    e.preventDefault();
+    //get the entry data from api and user input
+    try {
+        const entry = await getEntry();
+        //render the data on the DOM
+        renderData(entry);
+    } catch (err) {
+        renderData();
+    }
+})
